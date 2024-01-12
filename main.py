5c57d58ce5ff4769d484e0338e0d33bb6a93a0a4
Mohamed  Thoufeeq Rahman <thoufeeq87.mtr@gmail.com>
2024-01-12 23:45:06 +0400

    updated

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ if st.button("Generate Image"):
         "file_name": generated_file_name,
         "image": output,
     }
+    st.write(prompt_data)
 
     result = collection.insert_one(prompt_data)
     st.success(f"User prompts and generated image saved with ObjectID: {result.inserted_id}")
