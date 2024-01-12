f28e24f00a44a4d3e8227473cb789c85815510fc
Mohamed  Thoufeeq Rahman <thoufeeq87.mtr@gmail.com>
2024-01-12 20:50:26 +0400

    corrected the code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import openai
 import replicate
 import streamlit as st
@@ -40,7 +42,7 @@ if st.button("Generate Image"):
         messages=[{"role": "user", "content": user_prompt_file_name}]
     )
     generated_file_name = completion_file_name.choices[0].message.content.strip()
-    replicate.Client(api_token= st.secrets.replicate_key)
+    os.environ['REPLICATE_API_TOKEN'] = st.secrets.replicate_key
     # Generate image using OpenDALL·E
     output = replicate.run(
         "lucataco/open-dalle-v1.1:1c7d4c8dec39c7306df7794b28419078cb9d18b9213ab1c21fdc46a1deca0144",
